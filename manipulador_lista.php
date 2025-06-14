974df6ec36f4cb58a563ccbc5fc272631d422464
pduartedev <patrick.d.pimenta@gmail.com>
2025-06-14 18:53:27 -0300

    Feat: ordenar_decrescente, Fix: call lista ordenada

--- manipulador_lista.php
+++ manipulador_lista.php
@@ -10,6 +10,16 @@ function ordenar_crescente($lista)
     return $copia;
 }
 
+/**
+ *  Função que ordena a lista em ordem decrescente
+ */
+function ordenar_decrescente($lista)
+{
+    $copia = $lista;
+    // Usando a função rsort para ordenar em ordem decrescente
+    rsort($copia);
+    return $copia;
+}
 
 /**
  * Função principal que executa uma estratégia específica sobre a lista
@@ -23,10 +33,14 @@ function executar_estrategia($lista, $estrategia)
 $numeros = [4, 2, 7, 2, 1, 8, 4, 9, 6, 3];
 
 // Exibindo a lista original
-echo "Lista original: " . implode(", ", $numeros) . "\n";
+echo "Lista original: \t\t\t\t" . implode(", ", $numeros) . "\n";
 
 // Aplicando ordenação crescente
-$resultado = ordenar_crescente($numeros);
-echo "Lista ordenada: " . implode(", ", $resultado) . "\n";
+$resultado = executar_estrategia($numeros, 'ordenar_crescente');
+echo "Lista ordenada: \t\t\t\t" . implode(", ", $resultado) . "\n";
 
+
+// Aplicando ordenação decrescente
+$resultado = executar_estrategia($numeros, 'ordenar_decrescente');
+echo "Lista ordenada decrescente: \t" . implode(", ", $resultado) . "\n";
 ?>
