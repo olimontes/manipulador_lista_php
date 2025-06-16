fbfe4c3a126ea08fe0ab80ede404079dcf6cd744
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2025-06-16 18:21:11 -0300

    Update main.php
    atualizando o main com a função e teste de filtrar pares

--- main.php
+++ main.php
@@ -11,6 +11,13 @@ function ordenar_crescente($lista)
     return $copia;
 }
 
+//Função filtrar pares
+function filtrar_pares($lista) {
+    return array_filter($lista, function($num) {
+        return $num % 2 === 0;
+    });
+}
+
 /**
  * Função principal que executa uma estratégia específica sobre a lista
  */
@@ -28,6 +35,10 @@ echo "Lista original: " . implode(", ", $numeros) . "\n";
 // Aplicando ordenação crescente
 $resultado = ordenar_crescente($numeros);
 echo "Lista ordenada: " . implode(", ", $resultado) . "\n";
+
+//Filtra pares
+$resultado = executar_estrategia($lista, 'filtrar_pares');
+print_r($resultado);
 
 
 //teste da função remover_duplicatas
